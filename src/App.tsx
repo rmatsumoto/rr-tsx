744cdea304819a8f7c57bdb89de37f083915714a
rmatsumoto <yodamatsumoto@gmail.com>
2025-08-02 18:43:22 -0700

    transfer

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import App from './components/App';
+
+function Appp() {
+  return (
+    <App />
+  );
+}
+
+export default Appp;
---
+++ src/components/RepositoriesList.tsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { actionCreators } from "../state";
+
+const RepositoriesList: React.FC = () => {
+  const [term, setTerm] = useState('');
+  const dispatch = useDispatch();
+  
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    // dispatch(actionCreators.searchRepositories(term) as any);
+    dispatch(actionCreators.searchRepositories(term) as any);
+  }
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input type="text" value={term} onChange={e => setTerm(e.target.value)} />
+        <button>Search</button>
+      </form>
+    </div>
+  )
+}
+
+export default RepositoriesList;
